62db111056737a6cbaa61617862da27c3f4e6d39
Cythilya Tang <cythilya@gmail.com>
2014-11-23 16:17:55 +0800

    user page

--- microblog/routes/index.js
+++ microblog/routes/index.js
@@ -4,6 +4,14 @@
 //layout.ejs模板文件不是孤立展示的，默認情況下所有的模板都繼承自layout.ejs，<%- body %>部分才是獨特的內容，其他部分是共有的，可以看作是頁面框架。
 //使用模板引擎：res.render，並將其產生的頁面直接返回给客户端
 
+//Mockup
+var postList = [
+	{ id: 1, name: "Apple", msg: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the gre‬" },
+	{ id: 2, name: "Zoe", msg: "The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex! Fox nymph. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta." },
+	{ id: 3, name: "Cathy", msg: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu" }
+]; 
+var count = postList.length;
+
 //檢查使用者登入狀態
 var isLogin = false;
 var checkLoginStatus = function(req, res){
@@ -16,10 +24,10 @@ var checkLoginStatus = function(req, res){
 //首頁
 exports.index = function(req, res){
 	checkLoginStatus(req, res);
-	
 	res.render( 'index', {
 		title : '歡迎來到 Microblog', 
-		loginStatus : isLogin
+		loginStatus : isLogin,
+		posts : postList
 	});	
 };
 
@@ -77,30 +85,33 @@ exports.doLogin = function(req, res){
 exports.logout = function(req, res){
 	res.clearCookie('userid', { path: '/' });
 	res.clearCookie('password', { path: '/' });
-	console.log('dd');
 	return res.redirect('/');
 };
 
-//------------------------------------------------
+//發表訊息
+exports.post = function(req, res){
+	var element = { id: count++, name: req.signedCookies.userid, msg: req.body['post'] };
+	postList.push(element);
 
-
+	return res.redirect('/');	
+};
 
 //使用者頁面
 exports.user = function(req, res){
+	var userName = req.params.user;
+	var userPosts = [];
+	
+	for (var i = 0; i < postList.length; i++) { 
+		if(postList[i].name == userName){
+			userPosts.push(postList[i]);
+		}
+	}
+	
 	checkLoginStatus(req, res);
+	res.render( 'user', {
+		title : userName + '的頁面',
+		loginStatus : isLogin,
+		posts : userPosts
+	});
+	
 };
-
-//發表訊息頁面
-exports.post = function(req, res){
-	checkLoginStatus(req, res);
-};
-
-
-
-
-
-
-
-
-
-
